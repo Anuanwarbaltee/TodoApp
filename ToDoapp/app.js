e4fb1b6e50c748cfa191a0548e90a8d94db01f68
Muhammad Anwar <anwardines786@gmail.com>
2021-12-30 12:05:24 -0800

    work complete

---
+++ ToDoapp/app.js
@@ -0,0 +1,46 @@
+var todoInp = document.getElementById('todoInp')
+var showList = document.getElementById('showList')
+
+function addTodo() {
+
+    var todoText = todoInp.value
+
+    var todoTextNode = document.createTextNode(todoText)
+
+    var todoLi = document.createElement('li')
+    todoLi.setAttribute('class','todoList')
+    todoLi.appendChild(todoTextNode)
+    showList.appendChild(todoLi)
+
+
+    
+    var editBtn = document.createElement('button')
+    editBtn.setAttribute('onclick','editTodo(this)')
+    var editText = document.createTextNode('EDIT')
+    editBtn.appendChild(editText)
+    todoLi.appendChild(editBtn)
+    
+
+    var deleteBtn = document.createElement('button')
+    deleteBtn.setAttribute('onclick', 'deletedTodo(this)')
+    var deleteTextNode = document.createTextNode('DELETE')
+    deleteBtn.appendChild(deleteTextNode)
+    todoLi.appendChild(deleteBtn)
+
+
+    todoInp.value = ""
+
+    
+}
+
+function deletedTodo(element){
+    element.parentNode.remove()
+}
+
+function editTodo(element){
+    element.parentNode.firstChild.nodeValue = prompt('Edit Value',element.parentNode.firstChild.nodeValue)
+}
+
+function deleteAll(){
+    showList.innerHTML = ""
+}
